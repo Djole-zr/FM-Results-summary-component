4554f1e693a978bfe9a57bba5c279b5b2ee30a5f
Djole-zr <93897601+Djole-zr@users.noreply.github.com>
2023-12-16 00:54:11 +0100

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+Frontend Mentor
+# Frontend Mentor - Results summary component solution
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+### Links
+
+- Live Site URL:
+
+### Screenshot
+
+![preview-solution](Screenshot.png)
---
+++ app.js
@@ -0,0 +1,16 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((valuation, index) => {
+      const currentValuation = document.getElementById(`valuation${index + 1}`);
+
+      currentValuation.querySelector(".valuation-icon").src = valuation.icon;
+      currentValuation.querySelector(".category").textContent =
+        valuation.category;
+      currentValuation.querySelector(".final-score").textContent =
+        valuation.score;
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
+  });
