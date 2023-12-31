ab384343546d06d2134d4674857d430db9a63b75
Djole-zr <93897601+Djole-zr@users.noreply.github.com>
2023-12-31 08:45:46 +0100

    final score

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let score = 0
+let finalScore
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -5,13 +7,21 @@ fetch("data.json")
       const currentValuation = document.getElementById(`valuation${index + 1}`);
 
       currentValuation.querySelector(".valuation-icon").src = valuation.icon;
-      currentValuation.querySelector(".valuation-icon").alt = valuation.category;
+      currentValuation.querySelector(".valuation-icon").alt =
+        valuation.category;
       currentValuation.querySelector(".category").textContent =
         valuation.category;
       currentValuation.querySelector(".final-score").textContent =
         valuation.score;
+
+      score += valuation.score;
+      finalScore = Math.round(score / (index + 1));
+      document.querySelector('.circle-big-number').textContent = finalScore;
     });
   })
   .catch((error) => {
     console.error("Error fetching JSON:", error);
   });
+
+
+
